fa459510865c076f93463f970f6b6cb6659ef6c6
Zachary Sabourin <zachary.sabourin@gmail.com>
2020-05-09 17:51:24 -0400

    display clock

--- src/AlarmClock.cpp
+++ src/AlarmClock.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <iomanip>
 #include <time.h>
 #include "../headers/AlarmClock.h"
+
+using namespace std;
 
 namespace AlarmClock
 {
@@ -9,10 +12,11 @@ namespace AlarmClock
     private:
         time_t alarmTime;
         tm *tmOne;
+        int hours, minutes, seconds;
 
     public:
         AlarmClock(char *alarm)
-        {
+        {            
             if(strptime(alarm, "%b %d %r", tmOne) == NULL)
                 std::cout << "Error formatting time" << std::endl;
 
@@ -27,6 +31,25 @@ namespace AlarmClock
         double timeDifference()
         {
             return difftime(alarmTime, time(NULL));
+        }
+
+        void displayClock()
+        {
+            system("clear");
+
+            cout << setfill(' ') << setw(55) << "         TIMER         \n"; 
+            cout << setfill(' ') << setw(55) << " --------------------------\n"; 
+            cout << setfill(' ') << setw(29); 
+            cout << "| " << setfill('0') << setw(2) << hours << " hrs | "; 
+            cout << setfill('0') << setw(2) << minutes << " min | "; 
+            cout << setfill('0') << setw(2) << seconds << " sec |" << endl; 
+            cout << setfill(' ') << setw(55) << " --------------------------\n";
+        }
+
+        void countdown()
+        {
+
+            
         }
     };
 }
