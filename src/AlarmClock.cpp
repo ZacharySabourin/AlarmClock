8908bf18429be10ba983c8fbe6c491bee32f8bcb
Zachary Sabourin <zachary.sabourin@gmail.com>
2020-05-08 21:54:24 -0400

    intial code

---
+++ src/AlarmClock.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+class AlarmClock
+{
+    private:
+        double currentTime;
+        double alarmTime;
+        double remainingTime;
+
+    public:
+        AlarmClock()
+        {
+
+        }
+
+        ~AlarmClock()
+        {
+
+        }
+
+        int main()
+        {
+            return 0;
+        }
+};
